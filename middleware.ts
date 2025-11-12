185a6edb848c090aee72f65f7ab10309a43a5c94
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-11-12 19:14:03 +0300

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+// middleware.ts
+import createMiddleware from 'next-intl/middleware';
+import {locales, defaultLocale} from './src/i18n/config';
+
+export default createMiddleware({
+  locales,
+  defaultLocale
+});
+
+export const config = {
+  matcher: ['/((?!_next|.*\\..*).*)']
+};
