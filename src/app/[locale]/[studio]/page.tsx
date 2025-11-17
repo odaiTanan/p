520ff15961a46822d3e473ac52c55f5bf3451507
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-11-17 13:24:43 +0300

    Update page.tsx

--- src/app/[locale]/[studio]/page.tsx
+++ src/app/[locale]/[studio]/page.tsx
@@ -10,7 +10,8 @@ import {
   faBackward,
 } from "@fortawesome/free-solid-svg-icons";
 import ExternalNav from "@/components/ExternalNav";
-
+import { useTranslations } from "next-intl";
+import Myprojects from "@/components/MyProjects";
 interface PageProps {
   params: Promise<{ lang: string; studio: string }>;
 }
@@ -21,13 +22,18 @@ export default async function SitePage({ params }: PageProps) {
   const images = getImages(`${studio}`);
 
   return (
-    <div className="w-full min-h-screen relative overflow-hidden ">
+    <div className="w-full min-h-screen relative overflow-hidden bg-fixed ">
       <Fnisher>
         {" "}
         <ExternalNav />
-        <div className="pt-[150px] w-[90%] mx-auto">
+        <div className="pt-[150px]  pb-20 w-[90%] mx-auto flex flex-col gap-[100px]">
           {" "}
           <DynamicCarousel images={images} />
+          <Myprojects
+            project={
+              studio === "medicalcenter" ? 0 : studio === "restaurant" ? 2 : 1
+            }
+          />
         </div>{" "}
       </Fnisher>
     </div>
